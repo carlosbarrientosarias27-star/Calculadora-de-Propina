92463274177daeee41ce54887a9207f6965e2719
carlosbarrientosarias27-star <carlos.barrientosarias27@gmail.com>
2026-02-28 10:46:46 +0100

    Múltiples propinas

--- src/calculadora de propina.py
+++ src/calculadora de propina.py
@@ -6,10 +6,25 @@ def calcular_propina(total_cuenta):
     return propina 
 
 
+def calcular_propina(total_cuenta, porcentaje):
+    """
+    Calcula la propina según el porcentaje indicado.
+    Valida que el porcentaje sea positivo.
+    """
+    if porcentaje <= 0:
+        raise ValueError("El porcentaje debe ser un número positivo mayor que 0.")
+    
+    propina = total_cuenta * (porcentaje / 100)
+    return propina
+
+
 # Prueba manual
 total = 1000
-resultado = calcular_propina(total)
+porcentaje_propina = 10
+
+resultado = calcular_propina(total, porcentaje_propina)
 
 print("Total de la cuenta:", total)
-print("Propina (10%):", resultado)
+print(f"Propina ({porcentaje_propina}%):", resultado)
 print("Total a pagar:", total + resultado)
+
